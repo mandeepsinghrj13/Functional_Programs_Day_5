52f08aef8e344d0ade9302a9750e9a322666d6b3
mandeep <singh.mr.1996@gmail.com>
2021-06-27 19:21:21 +0530

    Two D Array

--- src/com/bridgelabz/TwoDArray.java
+++ src/com/bridgelabz/TwoDArray.java
@@ -1,8 +1,19 @@
 package com.bridgelabz;
 public class TwoDArray 
 {
-	public static void main(String[] args) 
-	{
-		System.out.println("InitiLize Setup For Functional Programs");
-	}
+	public static void main(String[] args)
+    {
+  
+        int[][] arr = { { 1, 2 }, { 3, 4 } };
+  
+        for (int i = 0; i < 2; i++) 
+        {
+            for (int j = 0; j < 2; j++) 
+            {
+                System.out.print(arr[i][j] + " ");
+            }
+  
+            System.out.println();
+        }
+    }
 }
